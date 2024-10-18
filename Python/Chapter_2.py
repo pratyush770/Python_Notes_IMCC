8322ef8ffeb6dc220eab8e392e88e68dff3531a6
pratyush majumdar <pratyushmajumdar27@gmail.com>
2024-10-18 10:17:44 +0530

    import file done

--- Python/Chapter_2.py
+++ Python/Chapter_2.py
@@ -4,7 +4,23 @@ class City:
         self.name = name  # self keyword refers to current instance
         self.population = population
 
+    def describecity(self):
+        print("In describeCity method...")
+        print(f"The name of the city is {self.name}")
+        print(f"The population of the city is {self.population}")
 
-mumbai = City("Mumbai", 1000000)
 
+class State:
+    def __init__(self, name):
+        self.name = name
+
+    def describestate(self):
+        print(f"The name of the state is {self.name}")
+
+
+# Object creation is not required for this file since we use this file in another file
+# mumbai = City("Mumbai", 1000000)
+# maharashtra = State("Maharashtra")
+# mumbai.describecity()
+# maharashtra.describestate()
 
---
+++ Python/UseFile.py
@@ -0,0 +1,6 @@
+# for import demonstration and file is Chapter_2.py
+from Chapter_2 import City, State
+mumbai = City("Mumbai", 1000000)
+maharashtra = State("Maharashtra")
+mumbai.describecity()
+maharashtra.describestate()
